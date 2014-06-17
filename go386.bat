8da92d66aef1ae31b8aea3e25c9f8ac0ad2f0989
benbearchen <benbearchen@gmail.com>
2014-06-17 18:51:25 +0800

    创建 go386.bat
    用于在 win64 系统下创建编译 386 的 go 程序环境

---
+++ go386.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM 本脚本用于在 win 64 系统下临时创建 go win 386 的编译环境
+REM
+REM 请设置 GOROOT386 为 go win 386 的目录
+REM 
+
+REM set GOARCH=386
+
+set GOROOT=%GOROOT386%
+
+REM 下面，请使用 %GOROOT%\bin\go 编译你的 go 程序
+
